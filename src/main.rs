bef9e6aeee06fe447f8d47225ca41de0661f595a
Carlos Perez-Lopez <carlos.perez-lopez@slamcore.com>
2023-04-26 22:47:47 +0200

    Advance with BIOS

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
 //mod basic_reader;
-mod cdrom;
+//mod cdrom;
+mod bios;
 
 //use basic_reader::read;
-use cdrom::CdRom;
+//use cdrom::CdRom;
+use bios::Bios;
 
 fn main() {
-    // Read to memory
-    let cdrom = CdRom::from_file("roms/tekken.bin").expect("Failed to read");
+    //// Read to memory
+    //let cdrom = CdRom::from_file("roms/tekken.bin").expect("Failed to read");
 
-    // ECC
-    cdrom.check_ecc();
+    //// ECC
+    //cdrom.check_ecc();
 
-    // Print info
-    let game_title = cdrom.get_title().expect("Something went wrong");
-    println!("Game Title: {}", game_title);
-    let game_developer = cdrom.get_developer().expect("Something went wrong");
-    println!("Game Developer: {}", game_developer);
-    let game_publisher = cdrom.get_publisher().expect("Something went wrong");
-    println!("Game Publisher: {}", game_publisher);
-    let game_platform = cdrom.get_platform().expect("Something went wrong");
-    println!("Game Platform: {}", game_platform);
+    //// Print info
+    //let game_title = cdrom.get_title().expect("Something went wrong");
+    //println!("Game Title: {}", game_title);
+    //let game_developer = cdrom.get_developer().expect("Something went wrong");
+    //println!("Game Developer: {}", game_developer);
+    //let game_publisher = cdrom.get_publisher().expect("Something went wrong");
+    //println!("Game Publisher: {}", game_publisher);
+    //let game_platform = cdrom.get_platform().expect("Something went wrong");
+    //println!("Game Platform: {}", game_platform);
 
-    // Images
-    cdrom.write_tim_images();
+    //// Images
+    //cdrom.write_tim_images();
+
+    // Read BIOS
+    let bios = Bios::from_file("bios/scph1001.bin").expect("Something went wrong!");
+
+    // Get hash
+    let hash = bios.get_hash();
+    println!("BIOS hash: {:x}", hash);
 }
