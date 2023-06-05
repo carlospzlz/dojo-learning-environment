0f9f8b85c571548d9cbcfcac7f39de07daa0fea8
Carlos Perez-Lopez <carlospzlz@gmail.com>
2023-06-05 21:29:54 +0200

    Fix issue in BEQ

--- src/system/cpu.rs
+++ src/system/cpu.rs
@@ -67,8 +67,8 @@ impl CPU {
         self.execute_instruction(bus);
 
         // 79400 - 79500
-        let start = 10000;
-        let amount = 100;
+        let start = 83875;
+        let amount = 10000;
         if self.state.cycle > start && self.state.cycle < (start + amount) {
             self.state.dump();
             println!("RAM SHA-256: {}", bus.get_ram_hash());
@@ -98,7 +98,8 @@ impl CPU {
     fn execute_instruction(&mut self, bus: &mut Bus) -> Result<(), String> {
         let instruction = &self.state.instruction;
         info!(
-            "{:x}  {} ({:?})",
+            "[Cycle={}] {:x}  {} ({:?})",
+            self.state.cycle,
             instruction.bits,
             instruction.to_string(),
             instruction.get_op_code()
@@ -233,7 +234,11 @@ impl CPU {
         let rs_value = self.state.registers.read_register(rs).unwrap();
         let rt_value = self.state.registers.read_register(rt).unwrap();
         if rs_value == rt_value {
-            self.state.registers.npc = self.state.registers.pc + (offset as u32);
+            // Sign extend to i32
+            let offset = (offset as i16 as i32) << 2;
+            // Add, PC interpreted as i32
+            let npc = self.state.registers.pc as i32 + offset;
+            self.state.registers.npc = npc as u32;
         }
     }
 
