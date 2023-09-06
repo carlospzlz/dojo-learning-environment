f174db42d1ca93f4ca15a6b14fdcf75cf22a1803
Carlos Perez-Lopez <carlospzlz@gmail.com>
2023-09-06 11:18:28 +0200

    Fist steps in TLE GUI

---
+++ src/tekken_learning_environment_gui.rs
@@ -0,0 +1,58 @@
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 500.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Tekken Learning Environment",
+        options,
+        Box::new(|_cc| Box::<MyApp>::default()),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::bottom("my_bottom_panel").show(ctx, |ui| {
+            ui.heading("Bottom Panel");
+            ui.heading("Bottom Panel");
+            ui.heading("Bottom Panel");
+            ui.heading("Bottom Panel");
+            ui.heading("Bottom Panel");
+        });
+        egui::SidePanel::left("my_left_panel").show(ctx, |ui| {
+            ui.heading("Left Panel");
+        });
+        egui::SidePanel::right("my_right_panel").show(ctx, |ui| {
+            ui.heading("Right Panel");
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+       });
+    }
+}
