a68e40a67bca406b1d05bcd9be2e769dd8ee1eaa
Carlos Perez-Lopez <carlospzlz@gmail.com>
2023-09-12 19:32:34 +0200

    Add controls' functionality to start, stop and next

--- src/psx_explorer.rs
+++ src/psx_explorer.rs
@@ -1,8 +1,8 @@
-use eframe::egui;
-use egui::{Color32, ColorImage, RichText};
-use image::{Rgb, RgbImage};
+use egui::{Color32, RichText, ColorImage};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use image::{RgbImage, Rgb};
+
 
 // Emu system
 mod gpu_viewer;
@@ -12,26 +12,14 @@ mod util;
 
 use psx::System;
 
-//fn load_image_from_memory(image_data: &[u8]) -> Result<ColorImage, image::ImageError> {
-//    let image = image::load_from_memory(image_data)?;
-//    let size = [image.width() as _, image.height() as _];
-//    let image_buffer = image.to_rgba8();
-//    let pixels = image_buffer.as_flat_samples();
-//    Ok(ColorImage::from_rgba_unmultiplied(
-//        size,
-//        pixels.as_slice(),
-//    ))
-//}
-
 fn main() -> () {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`)
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(1000.0, 600.0)),
-        //multisampling: 4,
-        //renderer: eframe::Renderer::Glow,
         ..Default::default()
     };
     eframe::run_native(
-        "Emu Explorer",
+        "RPSX GUI",
         options,
         Box::new(|cc| Box::new(MyApp::new(cc))),
     );
@@ -39,34 +27,28 @@ fn main() -> () {
 
 struct MyApp {
     system: System,
-    //rotating_triangle: Arc<Mutex<RotatingTriangle>>,
-    angle: f32,
-    texture: Option<egui::TextureHandle>,
+    is_running: bool,
+    next_frame: bool,
 }
 
 impl MyApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        //let gl = cc
-        //    .gl
-        //    .as_ref()
-        //    .expect("You need to run eframe with the glow backend");
         let bios_filepath = "bios/scph1001.bin";
         let game_filepath = "roms/tekken.bin";
         let mut system = System::new(&bios_filepath, &game_filepath);
-        //cc.egui_ctx.re
         // Register callbacks here
         system.reset();
         Self {
             system,
-            //rotating_triangle: Arc::new(Mutex::new(RotatingTriangle::new(gl))),
-            angle: 0.0,
-            texture: Default::default(),
+            is_running: true,
+            next_frame: false,
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
         egui::CentralPanel::default().show(ctx, |ui| {
             // Get frame buffer
             let (width, height) = self.system.get_display_size();
@@ -91,19 +73,12 @@ impl eframe::App for MyApp {
 
             // Load texture
             //let img = ColorImage::from_rgb([width, height], &framebuffer);
-            let img = image::imageops::resize(
-                &img,
-                new_width,
-                new_height,
-                image::imageops::FilterType::Lanczos3,
-            );
+            let img = image::imageops::resize(&img, new_width, new_height, image::imageops::FilterType::Lanczos3);
             let img = ColorImage::from_rgb([new_width as usize, new_height as usize], img.as_raw());
             let texture = ctx.load_texture(
-                "my-image",
-                //egui::ColorImage::example(),
-                //img.as_flat_samples(),
-                img,
-                Default::default(),
+                    "psx_screen",
+                    img,
+                    Default::default()
             );
 
             // Show frame
@@ -120,44 +95,32 @@ impl eframe::App for MyApp {
         });
 
         egui::SidePanel::left("my_left_panel").show(ctx, |ui| {
-            if ui.button("Boot").clicked() {
-                //register_repaint_callback(ctx, &mut self.system, Event::SystemBootComplete);
-                //self.system.boot();
-            }
             if ui.button("Start").clicked() {
-                //register_repaint_callback(ctx, &mut self.system, Event::SystemStartComplete);
-                //register_repaint_callback(ctx, &mut self.system, Event::InstructionComplete);
-                //self.system.start();
+                self.is_running = true;
             }
             if ui.button("Stop").clicked() {
-                //register_repaint_callback(ctx, &mut self.system, Event::SystemStopComplete);
-                //self.system.stop();
+                self.is_running = false;
             }
-            if ui.button("Next Instruction").clicked() {
-                //register_repaint_callback(ctx, &mut self.system, Event::InstructionComplete);
-                //self.system.next_instruction();
-            }
-            if ui.button("Shutdown").clicked() {
-                //register_repaint_callback(ctx, &mut self.system, Event::SystemShutdownComplete);
-                //self.system.shutdown();
+            if ui.button("Next").clicked() {
+                self.next_frame = true;
             }
             // File Controls
             ui.button("Load");
             ui.button("Save");
         });
         egui::SidePanel::right("my_right_panel").show(ctx, |ui| {
-            //ui.horizontal(|ui| {
-            //    if self.system.is_on() {
-            //        ui.label(RichText::new("⚡").color(Color32::YELLOW));
-            //    } else {
-            //        ui.label(RichText::new("⚡").color(Color32::GRAY));
-            //    }
-            //    if self.system.is_running() {
-            //        ui.label(RichText::new("⏺").color(Color32::LIGHT_GREEN));
-            //    } else {
-            //        ui.label(RichText::new("⏺").color(Color32::GRAY));
-            //    }
-            //});
+            ui.horizontal(|ui| {
+                //if self.system.is_on() {
+                //    ui.label(RichText::new("⚡").color(Color32::YELLOW));
+                //} else {
+                //    ui.label(RichText::new("⚡").color(Color32::GRAY));
+                //}
+                if self.is_running {
+                    ui.label(RichText::new("⏺").color(Color32::LIGHT_GREEN));
+                } else {
+                    ui.label(RichText::new("⏺").color(Color32::GRAY));
+                }
+            });
             ui.horizontal(|ui| {});
             ui.horizontal(|ui| {
                 ui.add_space(14.0);
@@ -182,7 +145,8 @@ impl eframe::App for MyApp {
                     self.system.get_controller().button_square = true;
                 }
                 ui.style_mut().visuals.override_text_color = Some(Color32::from_rgb(255, 102, 102));
-                if ui.button("○").clicked() {
+                if ui.button("○").clicked()
+                {
                     self.system.get_controller().button_circle = true;
                 }
             });
@@ -209,7 +173,12 @@ impl eframe::App for MyApp {
         });
 
         // Processing
-        self.system.run_frame();
+        if self.is_running {
+            self.system.run_frame();
+        } else if self.next_frame {
+            self.system.run_frame();
+            self.next_frame = false;
+        }
         self.system.get_controller().button_dpad_up = false;
         self.system.get_controller().button_dpad_down = false;
         self.system.get_controller().button_dpad_left = false;
@@ -222,151 +191,3 @@ impl eframe::App for MyApp {
         self.system.get_controller().button_select = false;
     }
 }
-
-//fn register_repaint_callback(
-//    ctx: &egui::Context,
-//    system: &mut system::System,
-//    event: system::Event,
-//) {
-//    let ctx_clone = ctx.clone();
-//    system.register_callback(event, move || {
-//        ctx_clone.request_repaint();
-//    });
-//}
-
-//impl MyApp {
-//    fn custom_painting(&mut self, ui: &mut egui::Ui) {
-//        let (rect, response) =
-//            ui.allocate_exact_size(egui::Vec2::new(640.0, 480.0), egui::Sense::drag());
-//
-//        self.angle += response.drag_delta().x * 0.01;
-//
-//        // Clone locals so we can move them into the paint callback:
-//        let angle = self.angle;
-//        let rotating_triangle = self.rotating_triangle.clone();
-//
-//        let callback = egui::PaintCallback {
-//            rect,
-//            callback: std::sync::Arc::new(egui_glow::CallbackFn::new(move |_info, painter| {
-//                rotating_triangle.lock().unwrap().paint(painter.gl(), angle);
-//            })),
-//        };
-//        ui.painter().add(callback);
-//    }
-//}
-//
-//struct RotatingTriangle {
-//    program: glow::Program,
-//    vertex_array: glow::VertexArray,
-//}
-//
-//impl RotatingTriangle {
-//    fn new(gl: &glow::Context) -> Self {
-//        use glow::HasContext as _;
-//
-//        let shader_version = if cfg!(target_arch = "wasm32") {
-//            "#version 300 es"
-//        } else {
-//            "#version 330"
-//        };
-//
-//        unsafe {
-//            let program = gl.create_program().expect("Cannot create program");
-//
-//            let (vertex_shader_source, fragment_shader_source) = (
-//                r#"
-//                    const vec2 verts[3] = vec2[3](
-//                        vec2(0.0, 1.0),
-//                        vec2(-1.0, -1.0),
-//                        vec2(1.0, -1.0)
-//                    );
-//                    const vec4 colors[3] = vec4[3](
-//                        vec4(1.0, 0.0, 0.0, 1.0),
-//                        vec4(0.0, 1.0, 0.0, 1.0),
-//                        vec4(0.0, 0.0, 1.0, 1.0)
-//                    );
-//                    out vec4 v_color;
-//                    uniform float u_angle;
-//                    void main() {
-//                        v_color = colors[gl_VertexID];
-//                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
-//                        gl_Position.x *= cos(u_angle);
-//                    }
-//                "#,
-//                r#"
-//                    precision mediump float;
-//                    in vec4 v_color;
-//                    out vec4 out_color;
-//                    void main() {
-//                        out_color = v_color;
-//                    }
-//                "#,
-//            );
-//
-//            let shader_sources = [
-//                (glow::VERTEX_SHADER, vertex_shader_source),
-//                (glow::FRAGMENT_SHADER, fragment_shader_source),
-//            ];
-//
-//            let shaders: Vec<_> = shader_sources
-//                .iter()
-//                .map(|(shader_type, shader_source)| {
-//                    let shader = gl
-//                        .create_shader(*shader_type)
-//                        .expect("Cannot create shader");
-//                    gl.shader_source(shader, &format!("{shader_version}\n{shader_source}"));
-//                    gl.compile_shader(shader);
-//                    assert!(
-//                        gl.get_shader_compile_status(shader),
-//                        "Failed to compile {shader_type}: {}",
-//                        gl.get_shader_info_log(shader)
-//                    );
-//                    gl.attach_shader(program, shader);
-//                    shader
-//                })
-//                .collect();
-//
-//            gl.link_program(program);
-//            assert!(
-//                gl.get_program_link_status(program),
-//                "{}",
-//                gl.get_program_info_log(program)
-//            );
-//
-//            for shader in shaders {
-//                gl.detach_shader(program, shader);
-//                gl.delete_shader(shader);
-//            }
-//
-//            let vertex_array = gl
-//                .create_vertex_array()
-//                .expect("Cannot create vertex array");
-//
-//            Self {
-//                program,
-//                vertex_array,
-//            }
-//        }
-//    }
-//
-//    fn destroy(&self, gl: &glow::Context) {
-//        use glow::HasContext as _;
-//        unsafe {
-//            gl.delete_program(self.program);
-//            gl.delete_vertex_array(self.vertex_array);
-//        }
-//    }
-//
-//    fn paint(&self, gl: &glow::Context, angle: f32) {
-//        use glow::HasContext as _;
-//        //unsafe {
-//        //    gl.use_program(Some(self.program));
-//        //    gl.uniform_1_f32(
-//        //        gl.get_uniform_location(self.program, "u_angle").as_ref(),
-//        //        angle,
-//        //    );
-//        //    gl.bind_vertex_array(Some(self.vertex_array));
-//        //    gl.draw_arrays(glow::TRIANGLES, 0, 3);
-//        //}
-//    }
-//}
