c6556bfcb0603eaf9cffb75752009d0dd447176f
Carlos Perez-Lopez <carlospzlz@gmail.com>
2023-09-26 20:57:35 +0200

    Attempt to parallelize search

--- src/q_learning.rs
+++ src/q_learning.rs
@@ -1,4 +1,6 @@
 use image::{DynamicImage, GrayImage, RgbImage};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use super::vision;
 
@@ -34,15 +36,29 @@ impl Agent {
 
     pub fn visit_state(&mut self, frame: RgbImage) {
         let frame = DynamicImage::ImageRgb8(frame).to_luma8();
-        for state in &mut self.states {
-            if vision::are_the_same(&state.frame, &frame) {
-                state.times_visited += 1;
+        if self.states.len() < 100
+        {
+            for state in &mut self.states {
+                if vision::are_the_same(&state.frame, &frame) {
+                    state.times_visited += 1;
+                    self.number_of_revisited_states += 1;
+                    self.last_visited_state = state.clone();
+                    return;
+                }
+            }
+        }
+        else {
+            let result = parallel_linear_search(self.states.clone(), frame.clone());
+            if let Some(index) = result {
+                let index = index;
+                self.states[index].times_visited += 1;
                 self.number_of_revisited_states += 1;
-                self.last_visited_state = state.clone();
+                self.last_visited_state = self.states[index].clone();
                 return;
             }
         }
-        let state = State {
+
+       let state = State {
             frame,
             times_visited: 0,
         };
@@ -64,6 +80,41 @@ impl Agent {
     }
 }
 
-fn parallel_linear_search(data: &[State], target: GrayImage) -> {
+fn parallel_linear_search(data: Vec<State>, target: GrayImage) -> Option<usize> {
+    println!("Parallel!");
+    let data = Arc::new(data);
+    let result = Arc::new(Mutex::new(None));
+    let target = Arc::new(target);
 
+    let chunk_size = data.len() / 4;
+    let mut handles = vec![];
+
+    for i in 0..4 {
+        let data_clone = Arc::clone(&data);
+        let result_clone = Arc::clone(&result);
+        let target_clone = Arc::clone(&target);
+        let handle = thread::spawn(move || {
+            let mut local_result = None;
+            let chunk = data_clone.chunks(chunk_size).nth(i).unwrap();
+            for (index, &ref state) in chunk.iter().enumerate() {
+                if vision::are_the_same(&state.frame, &target_clone) {
+                    local_result = Some(i * chunk_size + index);
+                    break;
+                }
+            }
+            // Lock the mutex to update result
+            let mut result = result_clone.lock().unwrap();
+            if result.is_none() {
+                *result = local_result;
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let result = result.lock().unwrap();
+    *result
 }
